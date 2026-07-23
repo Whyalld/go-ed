fe97d8b7936f5d4a60771e2505c9e56232884ee5
Orb <magamadov.said@inbox.ru>
2026-07-23 12:51:59 +0300

    some new stuff

--- 25_interface3/main.go
+++ 25_interface3/main.go
@@ -7,17 +7,28 @@ func main() {
 	printType("интерфейсы это легко")
 	printType([]string{"привет"})	
 
-	value := 3
+	var value any = 3
 	number, ok := value.(int)
 	fmt.Println(number, ok)
 }
 
-func printType(value interface{}) {
+func printType1(value interface{}) {
 	if _, ok := value.(int); ok {
 		fmt.Println("тип аргумента int")
 	} else if _, ok := value.(string); ok {
 		fmt.Println("тип аргумента string")
 	} else {
 		fmt.Println("тип аргумента не int и не string")
+	}
+}
+
+func printType(value interface{}) {
+	switch value.(type) {
+	case int:
+		fmt.Println("тип аргумента int")
+	case string:
+		fmt.Println("тип аргумента string")
+	default:
+		fmt.Println("неизвестный тип аргумента")
 	}
 }
---
+++ 26_any/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+// any равнозначем interface{}
+// эти 2 функции равнозначны
+func printValue(value interface{}) {
+	fmt.Println(value)
+}
+
+func printValue2(value any) {
+	fmt.Println(value)
+}
+
+func main() {
+	var value any = 5
+	// fmt.Println(value + 5) // Ошибка - value имеет тип any (interface{})
+
+	number, ok := value.(int)
+	
+	if ok {
+		fmt.Println(number + 5) // 10
+	}
+	// это называется type assertion
+}
+
+// функции double и double2 равнозначны, во второй показана инициализация переменный прямо в условии
+func double(value any) {
+	number, ok := value.(int)
+
+	if ok {
+		fmt.Println(number * 2)
+	} else {
+		fmt.Println("value не является числом")
+	}
+}
+
+func double2(value any) {
+	if number, ok := value.(int); ok {
+		fmt.Println(number * 2)
+	} else {
+		fmt.Println("value не является числом")
+	}
+}
+
+// Проверка нескольких типов
+// для этого обычно применяют type switch
+func describe(value any) {
+	switch v := value.(type) {
+	case int:
+		fmt.Println("int: ", v)
+	case string:
+		fmt.Println("string: ", v)
+	case bool:
+		fmt.Println("bool: ", v)
+	default:
+		fmt.Println("неизвестный тип: ", v)
+	}
+}
+// Конструкция .(type) используется только внутри type switch. Отдельно написать так нельзя:
+// v := value.(type) // ошбика

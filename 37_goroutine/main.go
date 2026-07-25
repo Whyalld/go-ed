14080a77f123b009ad0b0e05889479206be4fba7
Orb <magamadov.said@inbox.ru>
2026-07-25 23:40:46 +0300

    add channel

---
+++ 37_goroutine/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now() // время старта
+
+	fmt.Printf("Старт: %s\n", t.Format(time.RFC3339))
+
+	go func() {
+		for {
+			for _, r := range `-\|/` {
+				fmt.Printf("\r%c", r)
+				time.Sleep(time.Millisecond * 100)
+			}
+		}
+	}()
+
+	go calculateSomething(1000)
+
+	go calculateSomething(2000)
+
+	time.Sleep(8 * time.Second)
+
+	fmt.Printf("Время выполнения программы: %s\n", time.Since(t))
+}
+
+func calculateSomething(n int) {	
+	t := time.Now()
+
+	result := 0
+	for i := 0; i <= n; i++ {
+		result += i * 2
+		time.Sleep(time.Microsecond * 3)
+	}
+
+	fmt.Printf("Рузультат: %d; Прошло времени: %s\n", result, time.Since(t))
+}
